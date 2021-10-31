7defb6897106ecf84a9d207744ef49f6288c5034
RosalRicardo <ricardo.rosal01@gmail.com>
2021-10-31 08:53:41 -0300

    inclusao do wgna

--- src/RGA_comgpu.jl
+++ src/RGA_comgpu.jl
@@ -7,9 +7,9 @@ using UnicodePlots
 
 lr_g = 2e-4             # Taxa de aprendizadgem da rede geradora
 lr_d = 2e-4             # Taxa de aprendizadgem da rede discriminadora
-batch_size = 128        # tamanho do batch
-num_epochs = 1000       # Numero de Epocas de treino
-output_period = 100     # duracao do periodo dos graficos de treino da rede geradora
+batch_size = 8          # tamanho do batch
+num_epochs = 30       # Numero de Epocas de treino
+output_period = 10     # duracao do periodo dos graficos de treino da rede geradora
 n_features = 28 * 28    # Número de pixels em cada amostra do conjunto de dados MNIST
 latent_dim = 100        # Dimensão do espaço latente
 opt_dscr = ADAM(lr_d)   # Otimizador para o discriminador
@@ -18,7 +18,7 @@ opt_gen = ADAM(lr_g)    # Otimizador para o gerador
 # Carrega o dataset
 train_x, _ = MNIST.traindata(Float32);
 # essa dataset tem pixels ∈ [0:1]. Mapeando esses pixels para [-1:1]
-train_x = 2f0 * reshape(train_x, 28, 28, 1, :) .- 1f0;# |> gpu;
+train_x = 2f0 * reshape(train_x, 28, 28, 1, :) .- 1f0 |> gpu;
 #  DataLoader permite acessar dados em lote e lida com embaralhamento.
 train_loader = DataLoader(train_x, batchsize=batch_size, shuffle=true);
 
@@ -29,13 +29,13 @@ discriminator = Chain(Dense(n_features, 1024, x -> leakyrelu(x, 0.2f0)),
                         Dropout(0.3),
                         Dense(512, 256, x -> leakyrelu(x, 0.2f0)),
                         Dropout(0.3),
-                        Dense(256, 1, sigmoid))# |> gpu
+                        Dense(256, 1, sigmoid)) |> gpu;
 
 # Definição da rede geradora
 generator = Chain(Dense(latent_dim, 256, x -> leakyrelu(x, 0.2f0)),
                         Dense(256, 512, x -> leakyrelu(x, 0.2f0)),
                         Dense(512, 1024, x -> leakyrelu(x, 0.2f0)),
-                        Dense(1024, n_features, tanh))# |> gpu
+                        Dense(1024, n_features, tanh)) |> gpu;
 
 #Função de treinamento do discriminador
 function train_dscr!(discriminator, real_data, fake_data)
@@ -44,7 +44,7 @@ function train_dscr!(discriminator, real_data, fake_data)
     all_data = hcat(real_data, fake_data)
 
     # Vetor de destino para previsões: 1 para dados reais, 0 para dados falsos.
-    all_target = [ones(eltype(real_data), 1, this_batch) zeros(eltype(fake_data), 1, this_batch)];# |> gpu;
+    all_target = [ones(eltype(real_data), 1, this_batch) zeros(eltype(fake_data), 1, this_batch)] |> gpu;
 
     ps = Flux.params(discriminator)
     loss, pullback = Zygote.pullback(ps) do
@@ -63,7 +63,7 @@ end
 #Função de treinamento do gerador
 function train_gen!(discriminator, generator)
     # Vetor de ruido do espaço latente
-    noise = randn(latent_dim, batch_size)# |> gpu;
+    noise = randn(latent_dim, batch_size) |> gpu;
 
     # Defina os parâmetros e obtenha o retorno
     ps = Flux.params(generator)
@@ -91,7 +91,7 @@ for n in 1:num_epochs
         real_data = flatten(x);
 
         # Treina o discriminador
-        noise = randn(latent_dim, size(x)[end])# |> gpu
+        noise = randn(latent_dim, size(x)[end]) |> gpu;
         fake_data = generator(noise)
         loss_dscr = train_dscr!(discriminator, real_data, fake_data)
         loss_sum_dscr += loss_dscr
@@ -107,7 +107,7 @@ for n in 1:num_epochs
 
     if n % output_period == 0
         @show n
-        noise = randn(latent_dim, 4);# |> gpu;
+        noise = randn(latent_dim, 4) |> gpu;
         fake_data = reshape(generator(noise), 28, 4*28);
         p = heatmap(fake_data, colormap=:inferno)
         print(p)
